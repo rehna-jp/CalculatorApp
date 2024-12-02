ad204b9f44d9fa9130e56deefc1b5ee8e6396aa2
rehna-jp <jeremyrehna05@gmail.com>
2024-12-02 19:41:37 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./App.css"
+
+function App() {
+  return (
+    <>
+    <div className="container">
+        <div className="mother">
+          <div className="screen">
+            23
+          </div>
+          <div className="keypad">
+            <button>AC</button>
+            <button>DEL</button>
+            <button>+</button>
+          </div>
+        </div>
+    </div>
+    </>
+  );
+}
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Home sweet Home
+    </div>
+  )
+}
+
+export default Home
